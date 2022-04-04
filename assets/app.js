75b2d0d039d827dcef9c6b01147215ea8a5bc63d
Dhilip KARTHIKEYAN <74380649+K-Dhilip@users.noreply.github.com>
2022-04-04 14:17:48 +0200

    commit

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,6 @@
 let quotes = ['a', 'b', 'c', 'd', 'e', 'f'];
 let generate  = quotes[Math.floor(quotes.length * Math.random())];
 
-document.getElementById("output").innerHTML = "generate";
+// document.getElementById("output").innerHTML = generate;
+
+document.write(generate);
